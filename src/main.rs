e6bdc100a1706f126d65066011107a3fe49a4cf0
Ranadeep Biswas <ranadip.bswas@gmail.com>
2021-02-10 08:56:41 +0100

    file transfer over tcp

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use clap::{crate_authors, crate_description, crate_version};
-
-use std::convert::TryInto;
-
+use rand::{distributions::Alphanumeric, Rng};
+use std::{convert::TryInto, fs::File, net::TcpListener, net::TcpStream};
 use tokio_stream::StreamExt;
 
-use clap::Clap;
+use clap::{crate_authors, crate_description, crate_version, Clap};
+use serde::{Deserialize, Serialize};
 
-use rand::{distributions::Alphanumeric, Rng};
+use zenoh::{net, Value, Zenoh};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FileInfo {
+    name: String,
+    size: u64,
+    addr: String,
+}
 
 #[derive(Clap)]
 #[clap(version = crate_version!(), author = crate_authors!(), about = crate_description!())]
@@ -23,8 +29,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let opts: Opts = Opts::parse();
 
-    let config = zenoh::net::config::default();
-    let zenoh = zenoh::Zenoh::new(config).await?;
+    let config = net::config::default();
+    let zenoh = Zenoh::new(config).await?;
     let alias: String = rand::thread_rng()
         .sample_iter(&Alphanumeric)
         .take(4)
@@ -45,14 +51,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     } else {
         match my_stream.next().await {
             Some(change) => match change.value {
-                Some(zenoh::Value::StringUTF8(v)) => v,
+                Some(Value::StringUTF8(v)) => v,
                 _ => unreachable!(),
             },
             _ => unreachable!(),
         }
     };
 
-    println!("{} -> {}", alias, peer);
+    if let Some(filepath) = opts.filepath {
+        let listener = TcpListener::bind("0.0.0.0:0")?;
+        // TODO: resolve to interface address
+        let addr = listener.local_addr()?.to_string();
+
+        let name = std::path::Path::new(&filepath)
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .into();
+        let mut source = File::open(&filepath)?;
+        let size = source.metadata()?.len();
+
+        let finfo = FileInfo { name, size, addr };
+
+        log::info!("file info {:?}", finfo);
+
+        workspace
+            .put(
+                &(peer.as_str()).try_into()?,
+                bincode::serialize(&finfo)?.into(),
+            )
+            .await?;
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(target) => {
+                    let pb = indicatif::ProgressBar::new(size);
+                    std::io::copy(&mut source, &mut pb.wrap_write(target))?;
+                    break;
+                }
+                _ => unreachable!(),
+            }
+        }
+    } else {
+        let finfo: FileInfo = match my_stream.next().await {
+            Some(change) => match change.value {
+                Some(Value::Raw(zi, buf)) if zi == 0 => {
+                    bincode::deserialize(&buf.to_vec()).unwrap()
+                }
+                _ => {
+                    println!("{:?}", change);
+                    unreachable!()
+                }
+            },
+            _ => unreachable!(),
+        };
+        let mut source = TcpStream::connect(finfo.addr)?;
+        let target = File::create(finfo.name)?;
+        let pb = indicatif::ProgressBar::new(finfo.size);
+        std::io::copy(&mut source, &mut pb.wrap_write(target))?;
+    }
 
     Ok(())
 }
